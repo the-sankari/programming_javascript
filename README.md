954cffde17738b3d6801070353887e388507d498
Kajol Sutra dhar <kajol.777@gmail.com>
2024-01-15 13:54:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programming JavaScript
+Helsinki Business College
---
+++ task_1/script.js
@@ -0,0 +1,10 @@
+// alert("Welcome to leaening JavaScript!");
+console.log(`
+             ________________________
+             *                       *
+             * Learning is fun!      *
+             *                       *
+             ------------------------`);
+
+document.title = "Task 1 is done";
+
