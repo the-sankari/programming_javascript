02a1a2a5f1915e0cd37a2aa269d759739f0a37de
Kajol Sutra dhar <kajol.777@gmail.com>
2024-01-19 14:04:10 +0200

    basic function 01 task

---
+++ week2/exercises/basic_functions/basic_01.js
@@ -0,0 +1,10 @@
+// Kajol Sutra Dhar
+
+// Write the function sum such that it returns the sum of a and b.
+function sum(x , y) {
+    return x + y;
+}
+
+// sample usage - do not modify
+console.log(sum(1, 3));
+console.log(sum(2, 5));
