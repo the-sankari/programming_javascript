ab1c9445817c00a9afca349d192a62d7f409359c
Kajol Sutra dhar <kajol.777@gmail.com>
2024-01-19 14:07:10 +0200

    basic function 02 task

---
+++ week2/exercises/basic_functions/basic_02.js
@@ -0,0 +1,11 @@
+// Kajol Sutra Dhar
+
+// Write the function multiply such that it returns the product (the result of multiplication) of a and b.
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// sample usage - do not modify
+console.log(multiply(2, 4));
+console.log(multiply(3, 2));
