4d5aa4ce355701bd092e70d07991c6b1a25b81dc
Kajol Sutra dhar <kajol.777@gmail.com>
2024-01-22 16:28:47 +0200

    Exercise 08 submission

---
+++ week3/exercises/conditions/conditions_08.js
@@ -0,0 +1,23 @@
+// Kajol Sutra Dhar
+/*
+Compose a function called pluralize that:
+
+Accepts two arguments: a noun and a number.
+Returns a string combining the number and the appropriately pluralized form of the noun, such as '5 cats' or '1 dog'.
+
+ */
+
+function pluralize(noun, number) {
+  if (number > 1) {
+    return `${number} ${noun}s`;
+  } else if (number === 1) {
+    return `${number} ${noun}`;
+  } else {
+    return `no ${noun}`;
+  }
+}
+
+// Sample usage - do not modify
+console.log("I have " + pluralize("cat", 0));
+console.log("I have " + pluralize("dog", 1));
+console.log("I have " + pluralize("parrot", 7));
