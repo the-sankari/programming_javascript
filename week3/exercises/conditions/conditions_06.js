7d7250abc7255040e1b3827f64c3bf2984ea7620
Kajol Sutra dhar <kajol.777@gmail.com>
2024-01-22 16:00:07 +0200

    Exercise 06 submitted

---
+++ week3/exercises/conditions/conditions_06.js
@@ -0,0 +1,18 @@
+// Kajol Sutra Dhar
+/**
+Develop a function called greaterNum which:
+
+ - Accepts two arguments, both of which are numbers.
+ - Returns the larger of the two numbers.
+
+ */
+
+ function greaterNum(number1, number2) {
+    let largerNumber = number1 > number2 ? number1 : number2;
+   return largerNumber;
+ }
+
+// Sample usage - do not modify
+console.log(greaterNum(5, 10));
+console.log(greaterNum(2, 1));
+console.log(greaterNum(4, 25));
