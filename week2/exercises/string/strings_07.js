d11a5cf31a65a9443be8f6930e1003ac0616a9e4
Kajol Sutra dhar <kajol.777@gmail.com>
2024-01-22 13:16:35 +0200

    Ex 07 submitted

---
+++ week2/exercises/string/strings_07.js
@@ -0,0 +1,14 @@
+// Kajol Sutra Dhar
+/**
+Refine the concatInitials function to return a string that combines the firstNameInitial followed by the lastNameInitial.
+ */
+
+function concatInitials(firstNameInitial, lastNameInitial) {
+
+    return `${firstNameInitial[0]}${lastNameInitial[0]}`
+
+}
+
+// Sample usage - do not modify
+console.log(concatInitials('J', 'D')); // "JD"
+console.log(concatInitials('S', 'B')); // "SB"
