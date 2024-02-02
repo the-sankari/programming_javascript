71e6f7a3cd39725d9a1f959023b7e88b30082d34
Kajol Sutra dhar <kajol.777@gmail.com>
2024-02-02 13:03:51 +0200

    Basic dom 2

--- week4/04_exerices/basic_DOM/basic_DOM_2/script.js
+++ week4/04_exerices/basic_DOM/basic_DOM_2/script.js
@@ -10,5 +10,31 @@ When the button is clicked, the function should:
 Ensure the input field is cleared after adding the fruit to the list.
 
 */
+// Parent container
+const fruitList = document.querySelector("#fruitList");
 
+// Getting the input
+const input = document.querySelector("#fruitInput");
 
+// Add btn
+const addBtn = document.querySelector("#addFruitBtn");
+
+// Function to add new items
+function addFruitToList() {
+  // console.log(input.value);
+
+  // Creating new li element
+  const newItem = document.createElement("li");
+
+  // Assigning input value to the new li element
+  newItem.textContent = input.value;
+
+  // Appending new child to the fruitlist
+  fruitList.appendChild(newItem);
+
+  // Clearing the input area
+  input.value = "";
+}
+
+// Add click event to the add btn
+addBtn.addEventListener("click", addFruitToList);
