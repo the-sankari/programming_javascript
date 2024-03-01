477f4ebc13a2b131c127ea09ee9249412494518f
Kajol Sutra dhar <kajol.777@gmail.com>
2024-03-01 14:05:29 +0200

    Updating all file

--- week6/exerices/objects/objects_2.js
+++ week6/exerices/objects/objects_2.js
@@ -1,8 +1,28 @@
 /* Task 1
-Create a JavaScript object named `library` that represents a collection of books.
+Create a JavaScript array named `library` that represents a collection of books.
 Each book should have properties for `title`, `author`, and `yearPublished`. 
 Add at least two book objects to this collection.
 */
+
+const library = [
+  {
+    title: "Geetanjali",
+    author: "Rabindranath Tagore",
+    yearPublished: 1910,
+  },
+  {
+    title: "Agnibeena",
+    author: "Kazi Nazrul Islam",
+    yearPublished: 1922,
+  },
+  {
+    title: "Sherlock Holmes",
+    author: "Arthur Conan Doylee",
+    yearPublished: 1887,
+  },
+];
+
+console.log(`Task 1: ${library}`);
 
 // Your code here
 
@@ -10,8 +30,17 @@ Add at least two book objects to this collection.
 Access the `title` of the first book in the `library` collection and log it to the console.
 Modify the `yearPublished` for the second book in the collection to a new year, then log the updated book object to the console.
 */
-
 // Your code here
+
+console.log(`Task 2`);
+
+// const book1 = library[0];
+
+console.log(library[0].title);
+
+library[1].yearPublished = 1920;
+
+console.log(library[1]);
 
 /* Task 3
 Use dot notation to add a new property `genres` (an array of strings) to the first book in the `library`.
@@ -20,13 +49,32 @@ Use bracket notation to add a boolean property `isAvailable` to the same book, i
 
 // Your code here
 
+library[0].genres = ["poetry"];
+library[0]["isAvailable"] = true;
+console.log(`Task 3`);
+console.log(library[0]);
+
 /* Task 4
 Define a constructor function named `Book` that can create new book objects with properties for `title`, `author`, `yearPublished`, and `genres`.
 Using the `Book` constructor, create a new book object with the provided input values and add it to the `library` collection.
 */
 
 // Your code here
+console.log(`Task 4`);
+class Book {
+  constructor(title, author, yearPublished, genres) {
+    this.title = title;
+    this.author = author;
+    this.yearPublished = yearPublished;
+    this.genres = genres;
+  }
+}
 
+const book2 = new Book("Hamlet", "William Shakespeare", 1600, "drama");
+
+// console.log(book2);
+
+console.log(library);
 /* Task 5
 Write a function named `createBook` that takes parameters for `title`, `author`, `yearPublished`, and `genres` (an array).
 The function should return a new book object with these properties.
@@ -35,7 +83,33 @@ Test `createBook` by creating a new book object with user-provided input and log
 
 // Your code here
 
+function createBook(title, author, yearPublished, genres) {
+  return new Book(title, author, yearPublished, genres);
+}
+console.log(`Task 5`);
+const newBook = createBook("Hamlet", "William Shakespeare", 1600, [
+  "Drama",
+  "poet",
+]);
+console.log(newBook);
+
+// console.log(createBook(...library.genres));
+
+// const book5 = new createBook('Hamlet','William Shakespeare', 1600, ['drama']);
+
+// console.log(book5);
+
 /* Task 6
 Convert the `library` collection to a JSON string and log it to the console.
 Parse the JSON string back into a JavaScript object and log the first book's title to the console.
 */
+console.log(`
+
+Task 6
+
+`);
+
+const libraryJson = JSON.stringify(library);
+
+console.log(libraryJson);
+console.log(JSON.parse(libraryJson));
--- week7/07_exercises/car/app.js
+++ week7/07_exercises/car/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const addCarForm = document.querySelector("#add-car-form");
+console.log("Add car is triggered");
+const searchCarForm = document.querySelector("#serach-car-form");
 const cars = [];
 
 // Car class to make tamplate of car data
@@ -17,32 +19,39 @@ class Car {
 
 const addCarData = (e) => {
   e.preventDefault();
+  try {
+    const license = document.querySelector("#license").value;
+    const maker = document.querySelector("#maker").value;
+    const model = document.querySelector("#model").value;
+    const owner = document.querySelector("#owner").value;
+    const price = document.querySelector("#price").value;
+    const color = document.querySelector("#color").value;
 
-  const license = document.querySelector("#license").value;
-  const maker = document.querySelector("#maker").value;
-  const model = document.querySelector("#model").value;
-  const owner = document.querySelector("#owner").value;
-  const price = document.querySelector("#price").value;
-  const color = document.querySelector("#color").value;
+    if (!license || !maker || !model || !owner || !price || !color) {
+      throw new Error("input field should not be empty");
+      return;
+    }
+    //   create new car object
+    const newCar = new Car(license, maker, model, owner, price, color);
 
-  //   create new car object
-  const newCar = new Car(license, maker, model, owner, price, color);
+    // get the new car inside the cars array for reuse
+    cars.push(newCar);
+    console.table(cars);
 
-  // get the new car inside the cars array for reuse
-  cars.push(newCar);
-  console.table(cars);
-
-  displayTable();
-  // Reset the form inputs after car data added
-  addCarForm.reset();
+    displayTable();
+    // Reset the form inputs after car data added
+    addCarForm.reset();
+  } catch (error) {
+    console.error("Somethiing went wrong, try again!! ", error.message);
+  }
 };
 
 const displayTable = () => {
   const table = document.querySelector("#carsTable");
 
-//   table.innerHTML = table.rows[0].innerHtml;
+  //   table.innerHTML = table.rows[0].innerHtml;
   cars.forEach((car) => {
-    const row = table.insertRow(1);
+    const row = table.insertRow(-1);
     Object.values(car).forEach((text) => {
       const cell = row.insertCell(-1);
       cell.textContent = text;
@@ -50,4 +59,35 @@ const displayTable = () => {
   });
 };
 
+const searchCarByLicense = (e) => {
+  const displaySearch = document.querySelector("#searchResult");
+
+  e.preventDefault();
+  try {
+    const searchInput = document
+      .querySelector("#license-search")
+      .value.trim()
+      .toLowerCase();
+
+    if (!searchInput) {
+      throw new Error("Please provide a license number!!");
+    }
+
+    const result = cars.find(({ license }) =>
+      license.toLowerCase().includes(searchInput)
+    );
+
+    displaySearch.textContent = result
+      ? `Found ${license.maker}, ${license.model}, owned by ${license.owner} `
+      : "No car with that license plate";
+
+    console.log(displaySearch.textContent);
+  } catch (error) {
+    displaySearch.textContent = error.message;
+    displaySearch.style.color = "red";
+    console.error(error.message);
+  }
+};
+
 addCarForm.addEventListener("submit", addCarData);
+searchCarForm.addEventListener("submit", searchCarByLicense);
