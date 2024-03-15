a69326b2697b69a55c86c8b4cb4b0480025ebbac
Kajol Sutra dhar <kajol.777@gmail.com>
2024-03-15 12:45:10 +0200

    pokemon height and weight

--- week9/exercise/pokemon/main.js
+++ week9/exercise/pokemon/main.js
@@ -1,12 +1,31 @@
 let pokemons = [];
-const fetchData = async () => {
-  const response = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
-  );
-  const data = await response.json(); // make the string to json
-  // get the name from inside the results of the api
-  pokemons.push(...data.results);
-  displayData(data.results);
+// const fetchData = async () => {
+//   const response = await fetch(
+//     "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
+//   );
+//   const data = await response.json(); // make the string to json
+
+//   // get the name from inside the results of the api
+//   pokemons.push(...data.results);
+//   displayData(data.results);
+// };
+
+const fetchData = () => {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=500&offset=0")
+    .then((response) => response.json())
+    .then((json) => {
+      // get each element and data with maping
+      const fetches = json.results.map((item) => {
+        return fetch(item.url).then((res) => res.json());
+      });
+      Promise.all(fetches).then((data) => {
+        pokemons = data;
+        displayData(pokemons);
+        console.log(pokemons);
+      });
+      // pokemons = json.results;
+    })
+    .catch((error) => console.error(error));
 };
 
 fetchData();
@@ -17,7 +36,10 @@ const displayData = (data) => {
 
   data.forEach((pokemon) => {
     const pokemonCard = document.createElement("div");
-    pokemonCard.innerHTML = `<h2>${pokemon.name}</h2>`;
+    pokemonCard.innerHTML = `<h2>${pokemon.name}</h2>
+    <p>Height: ${pokemon.height/10} m</p>
+    <p>Weight: ${pokemon.weight/10} kg</p>
+    `;
     container.appendChild(pokemonCard);
   });
 };
@@ -32,12 +54,12 @@ const debounce = (func, delay) => {
   };
 };
 
-const searchPokemons = (search) => {
+const searchPokemons = debounce((search) => {
   const filteredData = pokemons.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(search.toLowerCase())
   );
   displayData(filteredData);
-};
+}, 300);
 
 document.querySelector("#searchInput").addEventListener("input", (e) => {
   searchPokemons(e.target.value);
